f24284aaf535f9f42bd95df1d3c5541aa95e1f5b
STechQ <stech.quick@yandex.com>
2024-12-13 10:24:12 +0000

    deploy for test

--- qui-lp/packages/websdk/src/init/VueInitter.d.ts
+++ qui-lp/packages/websdk/src/init/VueInitter.d.ts
@@ -1,10 +1,9 @@
-import Vue from 'vue';
 import { IDomElement } from "../../../../common/shrimp/interfaces/RenderingInterfaces/IDomElement";
+import Vue from 'vue';
 export declare class VueInitter {
     static postProcess(renderer: any): void;
     static GenerateRootInstance({ mountPoint }: {
         mountPoint: IDomElement;
     }): Vue;
-    private static addIconsStyle;
 }
 //# sourceMappingURL=VueInitter.d.ts.map
