822308123932a6b877171a02aacaf2a1b5610d7b
STechQ <stech.quick@yandex.com>
2024-12-26 11:09:11 +0000

    deploy for test

--- qui-lp/packages/websdk/src/components/quick/QAnimation.vue.d.ts
+++ qui-lp/packages/websdk/src/components/quick/QAnimation.vue.d.ts
@@ -1,20 +1,40 @@
 import Vue from "vue";
+import { AnimationItem } from "lottie-web";
 declare const _default: import("vue/types/vue").ExtendedVue<Vue, {
     lottieInstance: import("lottie-web").LottiePlayer;
+    lottieComp: AnimationItem;
 }, {
     loadAnimation(src: string | object): void;
+    play(): void;
+    stop(): void;
+    pause(): void;
+    setSpeedMethod(setSpeedData: number): void;
+    setDirectionMethod(direction: "left" | "right"): void;
     hibernate(): void;
     resurrect(): void;
-}, unknown, {
-    source: any;
-    height: number;
-    width: number;
+}, {
+    lottieStyle: {
+        position: "fixed" | "relative" | "static" | "absolute";
+        top: string;
+        bottom: string;
+        left: string;
+        right: string;
+        zIndex: string;
+    };
+    showTemplate: boolean;
+}, {
+    _renderingProps: any;
+    src: any;
+    loop: boolean;
+    autoplay: boolean;
+    speed: number;
+    direction: string;
     position: string;
-    top: string | number;
-    right: string | number;
-    bottom: string | number;
-    zIndex: string | number;
-    left: string | number;
+    top: string;
+    bottom: string;
+    left: string;
+    right: string;
+    zIndex: string;
 }>;
 export default _default;
 //# sourceMappingURL=QAnimation.vue.d.ts.map
