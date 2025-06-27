74f1fd92f3de9042875c2eddb2cfa387a6768eef
STechQ <stech.quick@yandex.com>
2025-06-27 10:21:12 +0000

    deploy for test

--- packages/websdk/src/managers/style/cssTagManager.d.ts
+++ packages/websdk/src/managers/style/cssTagManager.d.ts
@@ -3,9 +3,10 @@ export declare class CssTagManager {
     private readonly styleClass;
     private backupTags?;
     private timer;
+    private pageStateDictionary;
     constructor(styleClass: string);
-    hideStyleTags(debounceTimer: number, shadowRoot: ShadowRoot): void;
-    showStyleTags(debounceTimer: number, cssOrderManager: CssOrderManager): void;
+    hideStyleTags(debounceTimer: number, shadowRoot: ShadowRoot, pageName: string): void;
+    showStyleTags(debounceTimer: number, pageName: string, cssOrderManager: CssOrderManager): void;
     destroyStyleTagNames(shadowRoot: ShadowRoot): void;
 }
 //# sourceMappingURL=cssTagManager.d.ts.map
