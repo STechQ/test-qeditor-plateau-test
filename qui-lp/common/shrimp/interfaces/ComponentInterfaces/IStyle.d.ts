0fb6b58cfea8008c7a409fcb4fa7a239c7c9995c
STechQ <stech.quick@yandex.com>
2025-08-15 08:44:17 +0000

    deploy for test

--- qui-lp/common/shrimp/interfaces/ComponentInterfaces/IStyle.d.ts
+++ qui-lp/common/shrimp/interfaces/ComponentInterfaces/IStyle.d.ts
@@ -1,5 +1,5 @@
 import { IStyleChild } from "./ICompJson";
-export declare type StyleValue = Record<string, string | undefined | Record<string, string>>;
+export declare type StyleValue = Record<string, string | boolean | undefined | Record<string, string>>;
 export interface StyleItem {
     [key: string]: StyleValue | undefined;
     default?: StyleValue;
@@ -11,6 +11,7 @@ export interface IComponentInternalSI {
     groupName?: string;
     groupContent: Array<IGroupContent>;
     showComponent?: boolean;
+    inheritedStyle?: boolean;
 }
 export interface IGroupContent {
     styleKey: Array<IComponentStyleKey>;
