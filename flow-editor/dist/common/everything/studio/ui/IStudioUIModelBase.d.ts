5f19fc4645d4ea9b6bfc052e66a349c5bd35ebb0
STechQ <stech.quick@yandex.com>
2025-11-03 07:23:05 +0000

    deploy for test

--- flow-editor/dist/common/everything/studio/ui/IStudioUIModelBase.d.ts
+++ flow-editor/dist/common/everything/studio/ui/IStudioUIModelBase.d.ts
@@ -7,7 +7,7 @@ import { CertificateModelType, DataTypeModelType } from "../../dataType/runtimem
 import { IStudioUIObjectBase } from "./IStudioUIObjectBase";
 import { ConstantModelType } from "../../dataType/runtimemodels/types";
 import { SlaModelType } from "../../sla/runtimemodels/types";
-export type ContentType = "qjson" | "js" | "ts" | "yaml" | "css" | "png" | "jpg" | "jpeg" | "svg" | "gif" | "lottie" | "woff" | "woff2" | "ttf" | "otf" | "json" | "wsdl" | "xsd" | "theme" | "constant";
+export type ContentType = "qjson" | "js" | "ts" | "yaml" | "css" | "png" | "jpg" | "jpeg" | "svg" | "gif" | "lottie" | "woff" | "woff2" | "ttf" | "otf" | "json" | "wsdl" | "xsd" | "theme";
 export type ModelType = ContentType | "bpmn" | "process" | "entityDesigner" | "namedComponent" | FlowModelType | WorkflowModelType | RestServiceModelType | SoapServiceModelType | SoapServiceV2ModelType | "step" | /*"flowEntity" //FLOW ENTITY IS GOING BYE BYE (perhaps 🤔) |*/ "appSettings" | DataTypeModelType | EnumModelType | ConstantModelType | CertificateModelType | "erronous" | "theme" | JobSchedulerModelType | "container" | SlaModelType;
 export interface IStudioUIModelBase extends IStudioUIObjectBase {
     modelType: ModelType;
